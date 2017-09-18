272a7379166f698435bb398dcb7ab181c0bc7c95
Joshua Mumm <joshua.mumm@alaska.gov>
2017-09-18 13:48:38 -0800

    plot LM

--- code/2017T04TabsAndFigs.r
+++ code/2017T04TabsAndFigs.r
@@ -2,6 +2,10 @@
 
 #LOAD ----
 library(tidyverse)
+library(stats)
+library(plotrix)
+library (Hmisc)
+options (scipen = 10)
 dat_17 <- read.csv('./data/qP_simp_17_170916.csv') # using new size classes from 2017 SD op-plan
 dat_old <- read.csv('./data/qP_simp_oldSCs_170916.csv')# using the old pre-2017 size classes 
 
@@ -30,4 +34,52 @@ dat_old <- read.csv('./data/qP_simp_oldSCs_170916.csv')# using the old pre-2017 
   m <- m[,c(1,2,3,9,8,11,10,4:7)]
   m
   write.csv(m,'./output/931PopMales_Main_old.csv')
+ #################################################################################
+
+##Plot LM   
+  dat <- dat_old 
+  #Convert to thousands of crabs
+  dat$LM_P <- dat$LM_P_/1000
+  dat$LM_P_CI <- dat$LM_P_CI_/1000
+  
+  dat <- select(dat, "proj" = PROJECT_CODE, "yr" = YEAR,  LM_P, LM_P_CI)
+  
+  t04 <- dat[dat$proj == "T04",]
+  
+  #T04
+  par(mfrow=c(1,1), las=1, mgp=c(3.2, 1, 0), mar=c(3.8, 4.2, 1, 1), family = "serif", font = 1,
+      cex = 1, ps = 12, cex.lab = 1.16, bty = "l")
+  
+  #LM
+  dat <- t04
+  plotCI(dat$yr, dat$LM_P, col= "black", lwd=1,  pch= 19, cex = 1.0,
+         ui = dat$LM_P + dat$LM_P_CI,
+         li = ifelse((dat$LM_P - dat$LM_P_CI) > 0 , (dat$LM_P - dat$LM_P_CI), 0),
+         ylab= "Thousands of Crab", xlab = "", xaxt='n', yaxt='n', las = '1',font.lab = 2,
+         ylim = c(0,1750))
+  mtext("Year", side = 1, line = 2,font=2, cex= 1.16)
+  axis(side=1, at=seq(1990, 2015, by=1, las = '1', font = 2))
+  axis(side=2, at = seq(0, 1800, by=500, font = 2),
+       labels=formatC(seq(0, 1800, by=500),"d", big.mark=','))
+  minor.tick(ny = 5, nx = 0 )
+  
+  
+  
+  lines(x= c(2002,2014), y = c(500,500), lty = 5, col = "black", lwd =2)
+  lines(x= c(2002,2014), y = c(100,100), lty = 3, col = "dimgrey")
+  lines(x= c(2002,2014), y = c(50,50), lty = 5, col = "dimgrey")
+  
+  detach("package:dplyr", unload=TRUE)
+  library(stats)
+  rec<- dat[dat$yr > 1997,]
+  f5 <- rep(1/5, 5)
+  y_lag <- filter(rec$LM_P, f5, sides=1)
+  points(rec$yr, y_lag, col="dimgrey", cex = 1.0)
+  
+  legend ("topright", inset = .1,
+          legend = c("Commercial MSST","Noncommercial 5-year threshold", "Noncommercial 1-year threshold", "5-Year average"),
+          col = c("black","dimgrey","dimgrey", "dimgrey"),
+          lty = c(5,3,5, NA), pch = c(NA,NA,NA,1), lwd = c(2,1,1,NA), bty = "n", y.intersp =1.2)
+  
+  
   
