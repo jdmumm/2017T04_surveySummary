00b6038f90d13ac79085367226b43e51dac947e4
Joshua Mumm <joshua.mumm@alaska.gov>
2019-03-12 09:17:10 -0800

    expand t2s seperately from t1s also change from 5mm to .1 mm bins due to error w event 2013T04039 T2. (only 2 T2 awl, with similar cw)

--- code/bubblePlots.R
+++ code/bubblePlots.R
@@ -22,91 +22,136 @@ read.csv('./data/awl_shellfish_190301.csv') %>%
   filter (SPECIES_CODE == '931', SEX_CODE == 1) %>% 
   select (Event = EVENT_ID, 
           bw = BIOLOGICAL_WIDTH_MM, 
-          freq = FREQUENCY) -> awl 
+          freq = FREQUENCY,
+          type = SAMPLE_TYPE_CODE) -> awl 
 read.csv('./data/C_17_170921.csv') %>% 
-  select( Event = EVENT_ID,
-          tm = TM_T) -> tot
+  transmute( Event = EVENT_ID,
+          tm_1 = TM_A + TM_B,
+          tm_2 = TM_C + TM_D) -> tot
 
-# expand awl recs by freq - early years 1 rec represents >1 crab
-awl %>% uncount (freq) -> awl
+awl %>% uncount (freq) -> awl # expand awl recs by freq - early years 1 rec represents >1 crab
+awl %>% right_join (event) -> awl # filter awl by used in estimate  
 
-# filter awl by used in estimate  
-awl %>% right_join (event) -> awl 
-
-# Expand length freq by event ----
-
-#create empty df to hold final expanded awl 
-dat <-data.frame(Event=factor(), bw = numeric())
-
+##################################################################
+# EXPAND LENGTHS BY EVENT ----
+##################################################################
 events <- unique(awl$Event)  
 #i <- '2017T04040' # start w one event 
 
-for(i in events){    
-  Sys.sleep(0.1)
-  print ( c("####",paste (i, "   START") , "####"))
+# Type I  ----
+  dat_t1 <-data.frame(Event=factor(), bw = numeric()) #create empty df to hold final expanded awl 
   
-  # select 1 tow from awl 
-  awl %>% filter (Event == i) -> awl.i
-  
-  #num measured
-  m <- nrow(awl.i)
-  
-  #tot caught 
-  t <- tot[tot$Event == i, "tm"]
-  
-  
-  #create empty vectors for expanded size dists so that object exists even if no crab in it, prevents latter errors when combining
-  eawl <- numeric(0)
+  for(i in events){    
+    Sys.sleep(0.05)
+    print ( c("####",paste (i, "   START") , "####"))
     
-  #expand 
-  if(t > 0 & m > 1 & t - m > 1)   # conditional to prevent terminal errors 
-  {eawl <- expandLenFreq(awl.i$bw, w=5, total = t, decimals = 1) } # mb used width of .1 mm for catch up report. 
-  eawl # note these are only the additional, not measured crabs.
-  length(eawl) - (t - m) #compare num of expanded to tot caught minus measured.  Should = 0
+    awl %>% filter (Event == i, type == 1) -> awl.i # select 1 tow from awl . Chanage for T2s
   
-  #compare hists.  
-  par(mfrow= c(3,1))
-  hist (awl.i$bw, breaks = seq(0,200,5), main  = c("measured", i), freq = T, col = 'red')
-  hist (eawl, breaks = seq(0,200,5), main  = c("additional", i), freq = T, col = 'blue')
+    m <- nrow(awl.i) #num measured
   
-  #combine measured with additional lengths
-  all <- c(awl.i$bw, eawl)
+    t <- tot[tot$Event == i, "tm_1"] #tot caught . change for sub vs whole sample 
   
-  #compare total measurements in expanded awls to tot cnt from CC. Difs should equal 0. 
-  length(all) - t
-  
-  #assign event to vector of lenghts 
-  r <- length(all)
-  all.df <- data.frame(Event = as.factor(rep(i, r)),
-                     bw = all)
-  
-  # compare hists to first 2
-  hist (all.df$bw, breaks = seq(0,200,5), main  = c("combined", i), freq = T, col = 'purple')
-  
-  nrow(all.df) - t # compare num recs in expanded df to total from CC
-  
-  dat <- rbind(dat,all.df) # append to main df for all events 
-  
-  Sys.sleep(0.2) 
-  print ( c("####",paste (i, "   COMPLETE") , "####"))
-  }
-write.csv (dat,"output/expanded931male_Lengths.csv")
+    eawl <- numeric(0) #create empty vectors for expanded size dists so that object exists even if no crab in it, prevents latter errors when combining
+    
+    #expand 
+    if(t > 0 & m > 1 & t - m > 1)   # conditional to prevent terminal errors 
+    {eawl <- expandLenFreq(awl.i$bw, w=.1, total = t, decimals = 1) } # mb used width of .1 mm for catch up report. 5mm caused error w T2s on 2013T04039
+    eawl # note these are only the additional, not measured crabs.
+    length(eawl) - (t - m) #compare num of expanded to tot caught minus measured.  Should = 0
+    
+    #compare hists.  
+    par(mfrow= c(3,1))
+    hist (awl.i$bw, breaks = seq(0,200,5), main  = c("measured", i), freq = T, col = 'red')
+    hist (eawl, breaks = seq(0,200,5), main  = c("additional", i), freq = T, col = 'blue')
+    
+    #combine measured with additional lengths
+    all <- c(awl.i$bw, eawl)
+    
+    #compare total measurements in expanded awls to tot cnt from CC. Difs should equal 0. 
+    length(all) - t
+    
+    #assign event to vector of lenghts 
+    r <- length(all)
+    all.df <- data.frame(Event = as.factor(rep(i, r)),
+                       bw = all)
+    
+    # compare hists to first 2
+    hist (all.df$bw, breaks = seq(0,200,5), main  = c("combined", i), freq = T, col = 'purple')
+    
+    nrow(all.df) - t # compare num recs in expanded df to total from CC
+    
+    dat_t1 <- rbind(dat_t1,all.df) # append to main df for all events 
+    
+    Sys.sleep(0.1) 
+    print ( c("####",paste (i, "   COMPLETE") , "####"))
+    }
 
-## ERROR CHECKING                                                                                                  
+# Type II ----
+  dat_t2 <-data.frame(Event=factor(), bw = numeric()) #create empty df to hold final expanded awl 
+  
+  for(i in events){    
+    Sys.sleep(0.05)
+    print ( c("####",paste (i, "   START") , "####"))
+    
+    awl %>% filter (Event == i, type == 2) -> awl.i # select 1 tow from awl . Chanage for T2s
+    
+    m <- nrow(awl.i) #num measured
+    
+    t <- tot[tot$Event == i, "tm_2"] #tot caught . change for sub vs whole sample 
+    
+    eawl <- numeric(0) #create empty vectors for expanded size dists so that object exists even if no crab in it, prevents latter errors when combining
+    
+    #expand 
+    if(t > 0 & m > 1 & t - m > 1)   # conditional to prevent terminal errors 
+    {eawl <- expandLenFreq(awl.i$bw, w=.1, total = t, decimals = 1) } # mb used width of .1 mm for catch up report. 5mm created error on event 2013T04039
+    eawl # note these are only the additional, not measured crabs.
+    length(eawl) - (t - m) #compare num of expanded to tot caught minus measured.  Should = 0
+    
+    #compare hists.  
+    par(mfrow= c(3,1))
+    hist (awl.i$bw, breaks = seq(0,200,5), main  = c("measured", i), freq = T, col = 'red')
+    hist (eawl, breaks = seq(0,200,5), main  = c("additional", i), freq = T, col = 'blue')
+    
+    #combine measured with additional lengths
+    all <- c(awl.i$bw, eawl)
+    
+    #compare total measurements in expanded awls to tot cnt from CC. Difs should equal 0. 
+    length(all) - t
+    
+    #assign event to vector of lenghts 
+    r <- length(all)
+    all.df <- data.frame(Event = as.factor(rep(i, r)),
+                         bw = all)
+    
+    # compare hists to first 2
+    hist (all.df$bw, breaks = seq(0,200,5), main  = c("combined", i), freq = T, col = 'purple')
+    
+    nrow(all.df) - t # compare num recs in expanded df to total from CC
+    
+    dat_t2 <- rbind(dat_t2,all.df) # append to main df for all events 
+    
+    Sys.sleep(0.1) 
+    print ( c("####",paste (i, "   COMPLETE") , "####"))
+    } 
+  
+rbind (dat_t1, dat_t2)  -> dat_comb
+#write.csv (dat_comb,"output/expanded931male_Lengths.csv")
+
+## ERROR CHECKING ----                                                                                                  
   #compare totals
-  nrow(dat)
-  sum(tot %>% right_join (event) %>% select (tm))
+  nrow(dat_comb) 
+  sum(tot %>% right_join (event) %>% select (tm_1, tm_2)) # 32 fewer on expanded than sum of tot, presumably rounding or simialar
   
   ## compare histograms of awl to dat (combined measured + expanded)
   library (lattice)
   par(mfcol = c(2,1))
   # all
   hist (~ bw , data = awl, breaks = seq(0,200,1), main  = c("measured", "all tows"), freq = T, col = 'red' )
-  hist (~ bw , data = dat, breaks = seq(0,200,1), main  = c("expanded", "all tows"), freq = T, col = 'blue')
+  hist (~ bw , data = dat_comb, breaks = seq(0,200,1), main  = c("expanded", "all tows"), freq = T, col = 'blue')
 
-#______________________________________________________________________________________________________
-
+#############################################################################################################################
 ## bubble plot from expanded awl  ----   
+#############################################################################################################################  
 dat <-   read.csv ("output/expanded931male_Lengths.csv")
 dat$bw %>% cut_interval (width = 5, boundary = 0, labels = F, n= 40 ) -> dat$bin # bin
 dat$cp <- (dat$bin) *5 + 2.5
